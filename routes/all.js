3a79fd850b544f3245df6726712a41b9915c22e0
DIYgod <diy.d.god@gmail.com>
2017-04-19 21:39:15 +0800

    use SimpleCount

--- routes/all.js
+++ routes/all.js
@@ -1,4 +1,7 @@
-module.exports = function(req, res, next) {
+var https = require('https');
+
+module.exports = function (req, res, next) {
+    https.get(`https://api.prprpr.me/count/?id=DIYgod-Weibo2RSS&action=add`);
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Content-Length, Authorization, Accept, X-Requested-With , yourHeaderFeild');
     res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
